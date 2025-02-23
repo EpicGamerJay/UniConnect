9ba63532f9edd93dcc674ba68446c18b43d11a86
Jay Dhuri <jaydhuri22@gmail.com>
2025-02-23 20:29:16 +0530

    message model

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,8 +1,27 @@
 import mongoose from "mongoose";    
 
 const messageSchema = new mongoose.Schema({
-
-});
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    recieverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    messageText: {  
+        type: String,
+        required: true,
+    },
+    messageImage: {
+        type: String,
+        default: "",
+    },
+},
+{ timestamps: true }
+);
 
 const Message = mongoose.model('Message', messageSchema);
 export default Message;
